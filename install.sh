1f7bab3de44e7bf10c75562b6467b2c39c4263ee
HugoBunelier <bunelierpro@gmail.com>
2022-03-31 12:05:56 +0200

    add all

---
+++ install.sh
@@ -0,0 +1,57 @@
+# !/bin/bash
+
+
+#colors 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+install_zsh () {
+    if [ -f ~/.zshrc ]
+    then
+        echo " " >> ~/.zshrc
+        echo "alias epitool='bash ~/.epitool.sh'" >> ~/.zshrc
+        echo "${green}add to zshrc${reset}"
+    fi
+}
+
+install_bash () {
+    if [ -f ~/.bashrc ]
+    then 
+        echo " " >> ~/.bashrc
+        echo "alias epitool='bash ~/.epitool.sh'" >> ~/.bashrc
+        echo "${green}Done${reset}"
+    else
+        echo "${red}No bashrc file found${reset}"
+    fi
+}
+
+update () {
+    cp -fr .epitool.sh ~/.epitool.sh
+    echo "${green}Done${reset}"
+}
+
+#optin
+while getopts "u" option; do
+case $option in
+    u)
+        update
+        exit;;
+    \?)
+        echo "bad option"
+        exit;;
+    esac
+done
+
+#base
+echo "installation..."
+echo " "
+
+if grep -q "alias epitool" ~/.bashrc
+then 
+    echo "${red}Epitool est déjà installer${reset}"
+else
+    cp .epitool.sh ~/.epitool.sh
+    install_zsh
+    install_bash
+fi
